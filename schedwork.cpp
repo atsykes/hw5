e20134bfe5b1344d5fdf97c8fb67980f7beb6119
Andrew Sykes <atsykes@usc.edu>
2024-04-13 12:44:10 -0700

    HW5 Done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,11 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+void makeAll(const AvailabilityMatrix& avail, const size_t dailyNeed, DailySchedule& sched,map<size_t, vector<vector<Worker_T>>>& m, size_t day); // Creates a map of all possible workers for each day
+void scheduleOnDay(const AvailabilityMatrix& avail, const size_t dailyNeed, vector<Worker_T> sched, vector<vector<Worker_T>>& possibleSchedules,int day); // Creates all possible workers for a given day
+bool alreadyScheduled(int day, size_t nurse, vector<Worker_T> sched, size_t i); // Checks if a worker has already been scheduled on a day
+bool joinPossibleDays(map<size_t, vector<vector<Worker_T>>>& m, vector<size_t>& numShifts, const size_t& maxShifts, DailySchedule& sched, size_t day); // Given the possible schedules of each day, generates a possible schedule given the restrictions
+bool checkAndUpdateNumShifts(vector<Worker_T> nurses, vector<size_t>& numShifts, const size_t& maxShifts, size_t i); // Updates the amount of shifts that the nurses have
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -36,10 +41,98 @@ bool schedule(
         return false;
     }
     sched.clear();
+
     // Add your code below
+    vector<size_t> numShifts;
+    for (size_t i=0; i < avail[0].size(); ++i)
+        numShifts.push_back(0);
+    map<size_t, vector<vector<Worker_T>>> m;
+    makeAll(avail, dailyNeed, sched, m, 0);
 
-
-
-
+    return joinPossibleDays(m, numShifts, maxShifts, sched, 0);
 }
 
+void makeAll(const AvailabilityMatrix& avail, const size_t dailyNeed, DailySchedule& sched,map<size_t, vector<vector<Worker_T>>>& m, size_t day)
+{
+    if (day == avail.size()) return;
+
+    vector<Worker_T> dummy;
+    vector<vector<Worker_T>> possibleSchedules;
+    scheduleOnDay(avail, dailyNeed, dummy, possibleSchedules, day);
+
+    // Inserts the possible workers of each day into a map
+    pair<size_t, vector<vector<Worker_T>>> p(day, possibleSchedules);
+    m.insert(p);
+
+    makeAll(avail, dailyNeed, sched, m, day+1);
+    return;
+}
+
+
+void scheduleOnDay(const AvailabilityMatrix& avail, const size_t dailyNeed, vector<Worker_T> sched, vector<vector<Worker_T>>& possibleSchedules,int day)
+{
+    if (sched.size() == dailyNeed) 
+    {
+        possibleSchedules.push_back(sched);
+        return;
+    }
+
+    // Gets all possible combinations of nurses for each day
+    for (size_t nurse=0; nurse < avail[day].size(); ++nurse)
+    {
+        if (avail[day][nurse] && !alreadyScheduled(day, nurse, sched, 0) && sched.size() < dailyNeed)
+        {
+            sched.push_back(nurse);
+            scheduleOnDay(avail, dailyNeed, sched, possibleSchedules, day);
+            sched.pop_back();
+        }
+    }
+
+    // If it gets here, all possible combinations for 1 day have been found
+    return;
+}
+
+bool alreadyScheduled(int day, size_t nurse, vector<Worker_T> sched, size_t i)
+{
+    if (i >= sched.size()) return false;
+
+    if (nurse == sched[i]) return true;
+    else return alreadyScheduled(day, nurse, sched, i+1);
+}
+
+bool joinPossibleDays(map<size_t, vector<vector<Worker_T>>>& m, vector<size_t>& numShifts, const size_t& maxShifts, DailySchedule& sched, size_t day)
+{
+    map<size_t, vector<vector<Worker_T>>>::iterator it = m.find(day);
+    if (it == m.end()) return true;
+
+    for (size_t i=0; i < it->second.size(); ++i)
+    {
+        vector<Worker_T> nurses = it->second[i];
+        vector<size_t> alteredNumShifts = numShifts;
+        sched.push_back(nurses);
+        DailySchedule alteredSched = sched;
+
+        // Joins all possible workers of each day together, making sure that they are meeting the conditions
+        if (checkAndUpdateNumShifts(nurses, alteredNumShifts, maxShifts, 0))
+        {
+            if (joinPossibleDays(m, alteredNumShifts, maxShifts, alteredSched, day+1)) 
+            {
+                sched = alteredSched;
+                return true;
+            }
+        }
+        sched.pop_back();
+    }
+
+    return false;
+}
+
+bool checkAndUpdateNumShifts(vector<Worker_T> nurses, vector<size_t>& numShifts, const size_t& maxShifts, size_t i)
+{
+    if (i == nurses.size()) return true;
+
+    Worker_T current = nurses[i];
+    if (numShifts[current] + 1 > maxShifts) return false;
+    numShifts[current]++;
+    return checkAndUpdateNumShifts(nurses, numShifts, maxShifts, i+1);
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <deque>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +15,11 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void addStarsToFloating(std::string&, std::string&); // Adds stars to the floating string to represent the missing letters
+void permuteLetters(std::string&, std::deque<char>&, std::vector<std::string>&, size_t); // Permutes the characters in the floating string to the missing letters
+void makeDeque(const std::string&, std::deque<char>&, size_t); // Adds the characters of the floating string into a deque
+void wordleReplaceStars(const std::vector<std::string>&, std::set<std::string>&, const std::set<std::string>&); // Replaces the stars with possible letters and then adds valid words into a set
+void wordleReplaceStarsHelper(const std::string, std::set<std::string>&, const std::set<std::string>&, size_t); // Helper to wordleReplaceStars
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,95 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::string s = in;
+    std::string f = floating;
+    std::set<std::string> dest;
+    std::deque<char> d;
+    std::vector<std::string> vec;
+    addStarsToFloating(s, f);
+    makeDeque(f, d, 0);
+    permuteLetters(s, d, vec, 0);
+    wordleReplaceStars(vec, dest, dict);
+    
+    return dest;
 }
 
 // Define any helper functions here
+void addStarsToFloating(string& s, string& floating)
+{
+    size_t cnt = 0;
+    // Counts how many empty spots there are
+    for (size_t i=0; i < s.size(); ++i)
+    {
+        if (s[i] == '-') cnt++;
+    }
+    // Finds how many letters are missing and adds stars
+    cnt -= floating.size();
+    for (size_t j=0; j < cnt; ++j)
+    {
+        floating.push_back('*');
+    }
+}
+
+void makeDeque(const string& s, deque<char>& d, size_t depth)
+{
+    if (depth == s.size()) return;
+    d.push_back(s[depth]);
+    makeDeque(s, d, depth+1);
+}
+
+void permuteLetters(string& s, deque<char>& floating, vector<string>& vec, size_t depth)
+{
+    if (depth == s.size()) 
+    {
+        vec.push_back(s);
+        return;
+    }
+
+    // Looks for empty spots and permutes all of the floating characters
+    if (s[depth] == '-')
+    {
+        for (size_t k=0; k < floating.size(); ++k)
+        {
+            char assign = floating[0];
+            s[depth] = assign;
+            floating.pop_front();
+            permuteLetters(s, floating, vec, depth+1);
+            floating.push_back(assign);
+            s[depth] = '-';
+        }
+    }
+    else permuteLetters(s, floating, vec, depth+1);
+}
+
+
+void wordleReplaceStars(const vector<string>& v, set<string>& dest, const set<string>& dict)
+{
+    for (size_t i=0; i < v.size(); ++i)
+    {
+        wordleReplaceStarsHelper(v[i], dest, dict, 0);
+    }
+}
+
+void wordleReplaceStarsHelper(string s, set<string>& dest, const set<string>& dict, size_t depth)
+{
+    if (s.find('*') == string::npos)
+    {
+        if (dict.find(s) == dict.end()) return;
+
+        dest.insert(s);
+        return;
+    }
+
+    // Replaces the stars with possible characters
+    if (s[depth] == '*')
+    {
+        for (char h='a'; h < '{'; ++h)
+        {
+            s[depth] = h;
+            wordleReplaceStarsHelper(s, dest, dict, depth+1);
+            s[depth] = '-';
+        }
+    }
+    else wordleReplaceStarsHelper(s, dest, dict, depth+1);
+}
